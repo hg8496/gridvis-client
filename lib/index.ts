9420d53b72e9f33e81ef4db393d2fb68cb116d6a
Christian Stolz <hg8496@cstolz.de>
2019-05-26 21:50:24 +0200

    Added IDevice as export to main index

--- lib/index.ts
+++ lib/index.ts
@@ -1,3 +1,4 @@
 export { GridVisClient } from "./Client";
 export { IProject } from "./project/IProject";
+export { IDevice } from "./device/IDevice";
 export { RESTException } from "./RESTException";
